e21bf51cf58bfcc5157747f459bc91bec949bb0d
John <johnmsaylor@gmail.com>
2020-09-05 20:42:02 -0400

    added memes from SHUTUP server

--- commands/meme.js
+++ commands/meme.js
@@ -11,7 +11,18 @@ function getImages() {
     "https://i.imgur.com/1D5HnFW.jpg",
     "https://i.redd.it/a2ewhergumi51.jpg",
     "https://i.imgur.com/BStkWo8.png",
-    "https://i.redd.it/tgu8jba8hle51.jpg"
+    "https://i.redd.it/tgu8jba8hle51.jpg",
+    "https://media.discordapp.net/attachments/675019280311386131/675040931648372747/aqn150mcbcn01.png",
+    "https://media.discordapp.net/attachments/675019280311386131/693150587226947694/uySVm3M.jpg?width=655&height=666",
+    "https://media.discordapp.net/attachments/675019280311386131/675067117934215207/IuKuleC.jpg?width=667&height=667",
+    "https://media.discordapp.net/attachments/675019280311386131/675032307274481742/is6dq2962hx01.jpg",
+    "https://media.discordapp.net/attachments/675019223399006218/714418097561403414/3dc66ac.jpg?width=1185&height=666",
+    "https://media.discordapp.net/attachments/675019223399006218/675253984100876288/7r5poet0j9n21.jpg?width=671&height=666",
+    "https://media.discordapp.net/attachments/675019223399006218/675080508308324362/fkns5rztgflz.png?width=667&height=667",
+    "https://media.discordapp.net/attachments/675019223399006218/675064806805602327/x3au68u7y6b21.jpg?width=1185&height=666",
+    "https://media.discordapp.net/attachments/675019223399006218/675061239453581326/58e9865.png?width=667&height=667",
+    "https://media.discordapp.net/attachments/675019223399006218/675050793677619211/1da5cc4a2b7.gif",
+    "https://media.discordapp.net/attachments/675019223399006218/675048520696463371/bf1s1ycv70g11.png",
   ];
 
   return images;
