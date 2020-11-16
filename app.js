ff6871c8519992f518eb7eaf4102b57467045a4c
John <johnmsaylor@gmail.com>
2020-11-16 10:26:33 -0500

    updating channel get for v12 discord js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ client.on("ready", () => {
 });
 
 client.on('guildMemberAdd', member => {
-  client.channels.get('731531456844660737').send(":welcome2::welcome1:"); 
+  client.channels.cache.get('731531456844660737').send(":welcome2::welcome1:"); 
 });
 
 client.on("message", async (msg) => {
