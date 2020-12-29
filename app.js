00c17438ff7676aace9080957fbfefd33593575b
jmsaylor <johnmsaylor@gmail.com>
2020-12-29 07:46:42 -0500

    async bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const ms = require('ms');
+const ms = require("ms");
 const Discord = require("discord.js");
-const Canvas = require('canvas');
+const Canvas = require("canvas");
 const client = new Discord.Client();
 client.cmds = new Discord.Collection();
 
@@ -28,46 +28,59 @@ client.on("ready", () => {
   console.log(`You are ${client.user.tag}!`);
   client.user.setPresence({
     status: "online",
-    activity : {
-      name: "Elon is watching."
-    }
-  })
+    activity: {
+      name: "Elon is watching.",
+    },
+  });
 });
 
-client.on('guildMemberAdd', member => {
-  client.channels.cache.get('731531456844660737').send(":welcome2: :welcome1:"); 
-  const channel = member.guild.channels.cache.get('790925289387458570');
-	if (!channel) return;
+client.on("guildMemberAdd", async (member) => {
+  client.channels.cache.get("731531456844660737").send(":welcome2: :welcome1:");
+  const channel = member.guild.channels.cache.get("790925289387458570");
+  if (!channel) return;
 
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
+  const canvas = Canvas.createCanvas(700, 250);
+  const ctx = canvas.getContext("2d");
 
-  const background = await Canvas.loadImage('./background.png');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  const background = await Canvas.loadImage("./background.png");
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = "#74037b";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	// Slightly smaller text placed above the member's display name
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Total Server Members:,', canvas.width / 2.5, canvas.height / 3.5);
+  // Slightly smaller text placed above the member's display name
+  ctx.font = "28px sans-serif";
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(
+    "Total Server Members:,",
+    canvas.width / 2.5,
+    canvas.height / 3.5
+  );
 
-	// Add an exclamation point here and below
-	ctx.font = applyText(canvas, `${member.guild.memberCount}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+  // Add an exclamation point here and below
+  ctx.font = applyText(canvas, `${member.guild.memberCount}!`);
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(
+    `${member.displayName}!`,
+    canvas.width / 2.5,
+    canvas.height / 1.8
+  );
 
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
+  ctx.beginPath();
+  ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.clip();
 
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
+  const avatar = await Canvas.loadImage(
+    member.user.displayAvatarURL({ format: "jpg" })
+  );
+  ctx.drawImage(avatar, 25, 25, 200, 200);
 
-  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-  channel.lastMessage.delete()
+  const attachment = new Discord.MessageAttachment(
+    canvas.toBuffer(),
+    "welcome-image.png"
+  );
+  channel.lastMessage.delete();
   channel.send(attachment);
 });
 
@@ -97,8 +110,8 @@ client.on("message", async (msg) => {
 
 // Set to 15 minutes to avoid rate limit
 setInterval(() => {
-  let guild = client.guild.cache.get('700053942482239638');
+  let guild = client.guild.cache.get("700053942482239638");
   let members = guild.memberCount;
-  let channel = guild.channels.cache.get('790925289387458570');
+  let channel = guild.channels.cache.get("790925289387458570");
   channel.setName(`Members: ${members}`).catch(console.error);
-}, ms('15m'));
+}, ms("15m"));
