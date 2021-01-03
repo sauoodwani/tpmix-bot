51cfbcc8b8057dd5732da5b589508f80a5d43caa
Sauood <iam56@gmail.com>
2021-01-03 12:32:03 +0530

    No double teaming.

--- app.js
+++ app.js
@@ -34,22 +34,6 @@ client.on("ready", () => {
   });
 });
 
-const applyText = (canvas, text) => {
-  const ctx = canvas.getContext("2d");
-
-  // Declare a base size of the font
-  let fontSize = 70;
-
-  do {
-    // Assign the font to the context and decrement it so it can be measured again
-    ctx.font = `${(fontSize -= 10)}px sans-serif`;
-    // Compare pixel width of the text to the canvas minus the approximate avatar size
-  } while (ctx.measureText(text).width > canvas.width - 300);
-
-  // Return the result to use in the actual canvas
-  return ctx.font;
-};
-
 client.on("guildMemberAdd", async (member) => {
   let guild = client.guilds.cache.get("794610378171744337");
   let channel = guild.channels.cache.get("794610378867867680");
